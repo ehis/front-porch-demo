1c06299b0c8c0280a9cb2c93473c2b9eff76ae6f
Ehidiamen Ojielu <ehis@dialexa.com>
2016-10-24 10:53:16 -0500

    updated reactive component demo to show debouncing

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.css']
 })
-export class ReactiveComponent implements OnInit {
+export class ReactiveComponent implements OnInit, OnDestroy {
   private userForm: FormGroup;
+  private formSubscription: Subscription;
+  private controlSubscription: Subscription;
 
   constructor(fb: FormBuilder) {
     this.userForm = fb.group({
@@ -17,17 +21,27 @@ export class ReactiveComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.userForm.valueChanges
+    this.formSubscription = this.userForm.valueChanges
       .subscribe((value) => {
-        console.log(value)
+        console.log(value);
         const usernameControl = this.userForm.get('username');
         const passwordControl = this.userForm.get('password');
         console.log('username control', usernameControl);
         console.log('password control', passwordControl);
       });
+
+    this.controlSubscription =  this.userForm.get('username')
+      .valueChanges
+      .debounceTime(500)
+      .subscribe((value) => console.log('new value', value));
   }
 
   onSubmit() {
     console.log('reactive form', this.userForm.value);
+  }
+
+  ngOnDestroy () {
+    this.formSubscription.unsubscribe();
+    this.controlSubscription.unsubscribe();
   }
 }
